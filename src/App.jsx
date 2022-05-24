8d84d584bb52ec9f0216ab8bc7993f1ab5568bd8
goodjini <jinijoa0@gmail.com>
2022-05-25 00:15:47 +0900

    component 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
 import { InputTodo } from "./components/inputTodo";
+import { IncompleteTodos } from "./components/incompleteTodo";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
@@ -45,20 +46,11 @@ export const App = () => {
         onChange={onChangeTodoText}
         onClick={onClickAdd}
       />
-      <div className="complete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
       <div className="incomplete-area">
         <p className="title">完了のTODO</p>
         <ul>
